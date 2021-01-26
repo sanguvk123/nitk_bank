e7ec1b3d66da5b45d1d8add572ce646ea1114037
sanguvk123 <sanguvk123@gmail.com>
2021-01-27 01:40:56 +0530

    if not working remove homepage from views

---
+++ app/views/accounts/_account.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! account, :id, :account_id, :cos_id, :account_type, :account_no, :balance, :created_at, :updated_at
+json.url account_url(account, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :accounts
   resources :users
+  root 'homepage#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
