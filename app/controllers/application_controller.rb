d678ca518ffbcbe0cf5497ac5ea15663f1b66488
sanguvk123 <sanguvk123@gmail.com>
2021-02-04 03:01:51 +0530

    acc belongs to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    if session[:username]
-      @current_user ||= User.find(session[:username])
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
     else
       @current_user = nil
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader # Tells rails to use this uploader for this model.
   mount_uploader :sign, SignUploader # Tells rails to use this uploader for this model.
-  validates :name, presence: true, uniqueness: true# Make sure the owner's name is present.
+  validates :username, presence: true, uniqueness: true# Make sure the owner's name is present.
 end
